386f0e32eb05e20aab681ba0e9e7c1f43eb32302
Boston Dell-Vandenberg <boston@pomelodesign.com>
2015-06-03 07:32:17 -0400

    Only add alert class when a message exists

--- bootstrap-ninja-forms.js
+++ bootstrap-ninja-forms.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($) {
 
   $('.ninja-forms-field-error').addClass('help-block');
-  $('.ninja-forms-response-msg').addClass('alert alert-warning');
+  $('.ninja-forms-response-msg:not(:empty)').addClass('alert alert-warning');
   $('.ninja-forms-error-msg').removeClass('alert-warning').addClass('alert-danger');
   $('.ninja-forms-success-msg').removeClass('alert-warning').addClass('alert-success');
   $('.ninja-forms-required-items').addClass('alert alert-warning');
